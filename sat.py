2acb28d33b6fa8a80f03b90a005673d020de2c51
Jonathan <jonathan@bbs4.us>
2019-09-06 15:26:57 +0800

    windows binary fail

--- sat.py
+++ sat.py
@@ -65,7 +65,6 @@ def checkScores(usr, pwd):
         "sign-in": ""
         }
     s = requests.Session()
-    #print(headers)
     r = s.post(url, data=data, headers=headers)
     if r.status_code == 200:
         if "don\'t recognize" in r.text:
@@ -76,30 +75,24 @@ def checkScores(usr, pwd):
             logger.info("Login successful!")
     else:
         logger.error("Login failed! Exiting...")
-        # a non-200 response code means errors other than incorrect username/pwd
         logger.debug(r.text)
         exit(255)
     soup = BeautifulSoup(r.text, features="html.parser")
-    ## DEBUG:
     soup1 = soup
     scores = soup.find_all("div", {"class": "col-sm-7 col-xs-12 cb-base-font-size"})
     header_content = soup1.find_all("div", {"class":"header-content"})
-    #print(header_content)
     is_sat = []
     for i in range(0,len(header_content)):
         header_content_text = header_content[i].find_all("h3")[0].get_text()
-        #print(a)
         if header_content_text.find("SAT with Essay —") != -1 or a.find("SAT —") != -1:
             is_sat.append(True)
         else:
             is_sat.append(False)
-    #print(is_sat)
     offset = 0
     score_report = "\nYour scores:"
     score_sat = ""
     for i in range(0, len(header_content)):
         if is_sat[i] == False:
-            #print(i+offset)
             temp = header_content[i].find_all("h3")[0].get_text()
             temp2 = soup1.find_all("div", {"class":"score"})[i+offset].get_text()
             score_report += "\n"
@@ -120,15 +113,7 @@ def checkScores(usr, pwd):
             offset = new_offset
 
     logger.info(score_report)
-    '''for i in range(len(scores)):
-        tmp = scores[i].get_text()
-        tmp = tmp.replace("\n\n\n\n\n \n", "")
-        tmp = tmp.replace("\n\n\n", "")
-        tmp = tmp.replace(" \n", "")
-        tmp = tmp.replace("\n\n", "")
-        tmp = tmp.replace(" SAT", "SAT")
-        tmp = tmp.replace("Total Score", "")
-        logger.info(str(i+1) + ": " + tmp + "\n")'''
+
 
 def checkScoresDiff(usr, pwd):
     url = "https://account.collegeboard.org/login/authenticateUser"
@@ -148,7 +133,6 @@ def checkScoresDiff(usr, pwd):
         "password": pwd,
         "sign-in": ""
         }
-    #print(headers)
     s = requests.Session()
     r = s.post(url, data=data, headers=headers)
 
@@ -161,22 +145,18 @@ def checkScoresDiff(usr, pwd):
             logger.info("Login successful!")
     else:
         logger.error("Request failed with a status code of " + str(r.status_code) + ". Exiting...")
-        # a non-200 response code means errors other than incorrect username/pwd
         logger.debug(r.text)
         exit(255)
     soup = BeautifulSoup(r.text, features="html.parser")
     scores = soup.find_all("div", {"class": "col-sm-7 col-xs-12 cb-base-font-size"})
     header_content = soup.find_all("div", {"class":"header-content"})
     is_sat = False
-    #print(soup)
     header_content_text = header_content[0].find_all("h3")[0].get_text()
     if header_content_text.find("SAT with Essay —") != -1 or header_content_text.find("SAT —") != -1:
         is_sat = True
     else:
         is_sat = False
-    #print(is_sat)
     score_report = "\nYour scores:"
-    #score_sat = ""
     if is_sat == False:
         temp = header_content_text
         temp2 = soup.find_all("div", {"class":"score"})[0].get_text()
@@ -185,7 +165,6 @@ def checkScoresDiff(usr, pwd):
         score_report += ": "
         score_report += temp2
     else:
-        #new_offset = offset + 2
         temp = header_content_text
         score_report += "\n "
         score_report += temp
@@ -197,17 +176,6 @@ def checkScoresDiff(usr, pwd):
         score_report += soup.find_all("div", {"class":"score"})[2].get_text()
         offset = new_offset
 
-    #logger.info(score_report)
-    '''tmp1 = scores[0]
-    tmp = scores[0].get_text()
-    #print(scores[0])
-    tmp = tmp.replace("\n\n\n\n\n \n", "")
-    tmp = tmp.replace("\n\n\n", "")
-    tmp = tmp.replace(" \n", "")
-    tmp = tmp.replace("\n\n", "")
-    tmp = tmp.replace(" SAT", "SAT")
-    tmp = tmp.replace("Total Score", "")
-    res = tmp'''
     return score_report
 
 def main():
@@ -219,19 +187,16 @@ def main():
         args.check = True
         print("Checking new scores periodically. For help, add \"-h\" or \"--help\" for available options.")
     if args.check_once:
-        #print("oof")
         u, p = handle_config()
         checkScores(u, p)
         return
     if args.check:
         u, p = handle_config()
-        #print(p, u)
         prevResults = checkScoresDiff(u, p)
         logger.info(prevResults)
         time.sleep(20)
         iter = 0
         while True:
-            #print(p, u)
             curResults = checkScoresDiff(u, p)
 
             if curResults != prevResults:
